f7394d2b35368e770c88e045691dd16806d22a92
3x-haust <lyusungyun@icloud.com>
2026-06-26 14:22:28 +0900

    chore: use pure js password hashing

--- mimawiki_backend/src/auth/auth.service.ts
+++ mimawiki_backend/src/auth/auth.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import * as bcrypt from 'bcrypt';
+import * as bcrypt from 'bcryptjs';
 import { JwtService } from '@nestjs/jwt';
 import { User } from 'src/user/entities/user.entity';
 import { SignUpDto } from 'src/user/dto/sign-up.dto';
